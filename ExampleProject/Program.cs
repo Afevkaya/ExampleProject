a1e2759be5eaaf73d019de0930c1d0488b9c0f91
ahmet.evkaya <ahmet.evkaya@hangikredi.com>
2024-02-18 12:28:23 +0300

    Lesson-12 / Finished

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -1,6 +1,9 @@
+// builder uygulamaya servislerin eklenmesini sağlayan bir değişkendir
 var builder = WebApplication.CreateBuilder(args);
+
+// app uygulamada kullanılacak middleware/ara katman/ara yazılımların eklenmesini sağlayan değişkendir
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+app.Run(); 
