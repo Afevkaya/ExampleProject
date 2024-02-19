0ca3c618317197a2c0f41b6e0d8598a070717d22
ahmet.evkaya <ahmet.evkaya@hangikredi.com>
2024-02-19 09:33:20 +0300

    Lesson-17 / Finished

--- ExampleProject/Controllers/ProductController.cs
+++ ExampleProject/Controllers/ProductController.cs
@@ -1,14 +1,90 @@
+using System.Text.Json;
 using ExampleProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExampleProject.Controllers;
 
+#region NonController
+// Bazı durumlarda Controller classını requetlere kapatıp sadece uygulamadaki işlerde kullanmak gerekebilir.
+// Bu durumlarda [NonController] attribute ilgili controller'ı requestlere kapatır
+// [NonController]
+#endregion
 public class ProductController : Controller
 {
-    // public IActionResult GetProducts()
+
+    public IActionResult Index()
+    {
+        var products = new List<Product>
+        {
+            new Product {Id = 1, Name="A Product", Quantity = 100},
+            new Product {Id = 2, Name="B Product", Quantity = 200},
+            new Product {Id = 3, Name="C Product", Quantity = 300}
+        };
+        #region Model Bazlı Veri Gönderimi
+        // return View(products);
+        #endregion
+        
+        #region Veri Taşıma Kontrolleri
+        #region ViewBag
+        // View'a gönderilecek/taşınacak datayı dynamic şekilde tanımlanan bir değişken ile taşımamızı sağlayan veri taşıma kontrolüdür.
+
+        ViewBag.products = products;
+        #endregion
+
+        #region ViewData
+        // ViewBag'de olduğu gibi actşondakş datayı view'e taşımamızı sağlayan bir kontroldür
+        // Tek fark ViewData action atarfında boxing yapar ve cshtml tarafında unboxing yapmamızı ister
+
+        ViewData["products"] = products;
+        #endregion
+
+        #region TempData
+        // ViewData'de olduğu gibi actşondakş datayı view'e taşımamızı sağlayan bir kontroldür
+        // ViewData dan farklı olarak TempData tarayıcının cookie'si üzerinden taşınmaktadır
+        // Yani TempData üzerinden taşınan veriler başka Actionlar üzerinden de erişilebilir yani Actionlar üzerinde veri aktarımı TempData üzerinde yapılabilmektedir.
+
+        // string data = JsonSerializer.Serialize(products);
+        // TempData["products"] = data;
+        // return RedirectToAction("Index2");
+        #endregion
+
+        return View();
+        #endregion
+
+        
+    }
+
+    #region TempData
+    // public IActionResult Index2()
     // {
-    //     return View();
+    //     var data = TempData["products"].ToString();
+    //     var products = JsonSerializer.Deserialize<List<Product>>(data);
+    //     return View(products);
     // }
+
+    #endregion
+
+
+
+    #region GetProducts
+    public IActionResult GetProducts()
+    {
+        return View();
+    }
+    #endregion
+
+    #region NonAction
+    // Controller sınıfı içindeki her metod uygulama  tarafından bir action metod olarak algılanır
+    // Bu da demektir ki istek(request) alabilir bir metottur
+    // Controller içinde istenmese de bazı durumlarda business işlerin yürütüldüğü metotlar bulunabilir.
+    // Bu metotlar business ile ilgili işlemler yürtüütğü için requestlere kapalı olması istenir.
+    // İşte bu durumlarda [NonAction] attribute ilgili metodu requestlere kapatır
+    // [NonAction]
+    // public void X()
+    // {
+
+    // }
+    #endregion
 
     #region ViewResult
 
@@ -88,10 +164,10 @@ public class ProductController : Controller
 
     // AcitonResult geri dönüş tipinin nterface'i arayüzüdür
     // Gelen istekler sonucunda response olarak client'a dönülecek tip değişkenlik göstermesi durumunda kullanılan geri dönüş tipidir.
-    public IActionResult GetProducts()
-    {
-        return View();
-    }
+    // public IActionResult GetProducts()
+    // {
+    //     return View();
+    // }
     #endregion
 
 }
